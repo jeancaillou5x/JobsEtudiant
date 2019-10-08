6c595dc05994d7555786b2d2225e60c54ccc1bd5
loxfer72 <56304676+loxfer72@users.noreply.github.com>
2019-10-08 15:10:20 +0200

    Add files via upload

--- JobsEtudiants/vue/vue_entete.php
+++ JobsEtudiants/vue/vue_entete.php
@@ -1,23 +1,26 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" >
-    <head>
-        <title>accueil</title>
-       	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    </head>
+<head>
+  <title>accueil</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
 <nav class="navbar navbar-expand-lg navbar-light bg-light nav">
-  <a class="navbar-brand" href="index.php">
-  	<img src="images/logo.png">
-  </a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  	<div class="mx-auto nav">
-    <li class="nav-item justify-content-center">
-    	<a class="nav-link active" href="#">Liste des emplois</a>
-  	</li>
-  	<li class="nav-item justify-content-center">
-    	<a class="nav-link active" href="#">Liste des CV</a>
- 	</li>
+  <div class="col-1">
+    <a class="navbar-brand" href="index.php">
+     <img src="images/logo.png">
+   </a>
  </div>
-  
+ <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+  <span class="navbar-toggler-icon"></span>
+</button>
+<div class="mx-auto nav">
+  <li class="nav-item justify-content-center">
+   <a class="nav-link active" href="#">Liste des emplois</a>
+ </li>
+ <li class="nav-item justify-content-center">
+   <a class="nav-link active" href="#">Liste des CV</a>
+ </li>
+</div>
+<div class="col-1"> 
+</div>
 </nav>
 </html>
