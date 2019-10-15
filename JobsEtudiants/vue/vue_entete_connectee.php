4cdb095ec35e24921b826aba47acc3dbe07c1ae5
loxfer72 <56304676+loxfer72@users.noreply.github.com>
2019-10-15 15:31:58 +0200

    Add files via upload

---
+++ JobsEtudiants/vue/vue_entete_connectee.php
@@ -0,0 +1,51 @@
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" >
+<head>
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>	
+  <title>accueil</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light nav">
+
+  <div class="col-2" name="logo">
+    <a class="navbar-brand" href="index.php">
+     <img src="images/logo.png">
+   </a>
+ </div>
+
+ <div class="mx-auto nav" name="Les listes">
+  <ul class="nav nav-tabs">
+    <li class="nav-item justify-content-center">
+     <a type="button" class="nav-link btn btn-primary btn-lg btn-block" href="#">Liste des emplois</a>
+   </li>
+   <li class="nav-item justify-content-center">
+    <a type="button" class="nav-link btn btn-primary btn-lg btn-block" href="#">Liste des CV</a>
+  </li>
+</ul>
+</div>
+
+<div class="dropdown col-2" name="Espace Connexion">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    <img src="images/utilisateur.png">
+  </button>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    <a class="dropdown-item" href="#">Espace personnel</a>
+    <a class="dropdown-item" href="#">Déconnection</a>
+  </div>
+</div>
+
+</nav>
+
+<div class="float-left fixed-bottom text-dark" name="Navbar du bas" style="margin-top:">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light nav">
+    <p style="margin-bottom:0px;">Vous avez une question ?</p>
+    <li class="nav-item justify-content-center">
+     <a class="nav-link active" href="#">Support</a>
+   </li>
+ </nav>
+</div>
+<body class="bg-info text-white">
+</body>
+</html>
