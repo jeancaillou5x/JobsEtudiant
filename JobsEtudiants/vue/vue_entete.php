fcc5a180729bd6c9ddadedf62983b0a67a9285b0
loxfer72 <56304676+loxfer72@users.noreply.github.com>
2019-10-15 14:33:02 +0200

    Add files via upload

--- JobsEtudiants/vue/vue_entete.php
+++ JobsEtudiants/vue/vue_entete.php
@@ -1,26 +1,47 @@
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" >
+﻿<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" >
 <head>
   <title>accueil</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
+
 <nav class="navbar navbar-expand-lg navbar-light bg-light nav">
-  <div class="col-1">
+
+  <div class="col-1" name="logo">
     <a class="navbar-brand" href="index.php">
      <img src="images/logo.png">
    </a>
  </div>
- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-  <span class="navbar-toggler-icon"></span>
-</button>
-<div class="mx-auto nav">
-  <li class="nav-item justify-content-center">
-   <a class="nav-link active" href="#">Liste des emplois</a>
- </li>
- <li class="nav-item justify-content-center">
-   <a class="nav-link active" href="#">Liste des CV</a>
- </li>
+
+ <div class="mx-auto nav" name="Les listes">
+  <ul class="nav nav-tabs">
+    <li class="nav-item justify-content-center">
+     <a type="button" class="nav-link btn btn-primary btn-lg btn-block" href="#">Liste des emplois</a>
+   </li>
+   <li class="nav-item justify-content-center">
+    <a type="button" class="nav-link btn btn-primary btn-lg btn-block" href="#">Liste des CV</a>
+  </li>
+</ul>
 </div>
-<div class="col-1"> 
+
+<div class="col-1" name="Espace Connexion">
+  <a href="vue_connexion">
+    <button>Connexion</button>
+  </a>
+  <a href="vue_connexion">
+    <button>Créer un compte</button>
+  </a>
 </div>
+
 </nav>
+
+<div class="float-left fixed-bottom text-dark" name="Navbar du bas" style="margin-top:">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light nav">
+    <p style="margin-bottom:0px;">Vous avez une question ?</p>
+    <li class="nav-item justify-content-center">
+     <a class="nav-link active" href="#">Support</a>
+   </li>
+ </nav>
+</div>
+<body class="bg-info text-white">
+</body>
 </html>
